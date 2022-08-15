1c24d2af703a91e1a250e125b0565ff0510b3be7
nehal.abubakar <8qF9KShNXeZs36QTMFgD>
2022-08-15 17:22:30 +0500

    Init

--- layout/script.php
+++
@@ -1,9 +0,0 @@
-<script src="js/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
-<script src="js/jquery.fancybox.min.js"></script>
-<script src="js/slick.min.js"></script>
-<script src="js/custom.min.js"></script>
-</body>
-
-</html>
