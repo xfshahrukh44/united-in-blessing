8c81ce6349a825778d362457b373c2ea8e0b43db
Nehal Abubakar <nehal.abubakar@olivedigital.com>
2022-10-03 17:45:14 +0500

    Amendments on signup page.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,7 +24,7 @@
                             </div>
                             <div class="col-md-6 mb-4">
                                 <input type="text" class="form-control" placeholder="Please Enter Inviter Username"
-                                       name="inviters_username" required>
+                                       name="inviters_username" value="{{ old('inviters_username') }}" required>
                                 @error('inviters_username')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -32,7 +32,7 @@
                                 @enderror
                             </div>
                             <div class="col-md-6 mb-4">
-                                <input type="text" class="form-control" placeholder="UserName" name="username" required>
+                                <input type="text" class="form-control" placeholder="UserName" name="username" value="{{ old('username') }}" required>
                                 @error('username')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -40,7 +40,7 @@
                                 @enderror
                             </div>
                             <div class="col-md-6 mb-4">
-                                <input type="text" class="form-control" placeholder="Please Enter Your First Name" name="first_name"
+                                <input type="text" class="form-control" placeholder="Please Enter Your First Name" name="first_name" value="{{ old('first_name') }}"
                                        required>
                                 @error('first_name')
                                 <span class="invalid-feedback" role="alert">
@@ -49,7 +49,7 @@
                                 @enderror
                             </div>
                             <div class="col-md-6 mb-4">
-                                <input type="text" class="form-control" placeholder="Please Enter Your Last Name" name="last_name"
+                                <input type="text" class="form-control" placeholder="Please Enter Your Last Name" name="last_name" value="{{ old('last_name') }}"
                                        required>
                                 @error('last_name')
                                 <span class="invalid-feedback" role="alert">
@@ -58,7 +58,7 @@
                                 @enderror
                             </div>
                             <div class="col-md-6 mb-4">
-                                <input type="email" class="form-control" placeholder="Email Address" name="email"
+                                <input type="email" class="form-control" placeholder="Email Address" name="email" value="{{ old('email') }}"
                                        required>
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -67,7 +67,7 @@
                                 @enderror
                             </div>
                             <div class="col-md-6 mb-4">
-                                <input type="tel" class="form-control" placeholder="123-456-7890" name="phone" required>
+                                <input type="tel" class="form-control" placeholder="123-456-7890" name="phone" value="{{ old('phone') }}" required>
                                 @error('phone')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -94,7 +94,7 @@
                             </div>
                             <div class="mb-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+                                    <input class="form-check-input" type="checkbox" value="1" id="flexCheckDefault"
                                            name="accept" required>
                                     <label class="form-check-label" for="flexCheckDefault">Please confirm that you are a US resident in order to proceed</label>
                                 </div>
