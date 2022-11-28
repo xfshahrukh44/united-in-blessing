21eb135d555f412fa041a7b8487b51ed68f331cb
samgarcia14433 <108543947+samgarcia14433@users.noreply.github.com>
2022-11-29 03:53:31 +0500

    revision updated

--- resources/views/gifting-form.blade.php
+++ resources/views/gifting-form.blade.php
@@ -26,8 +26,7 @@
                 Back
             </a>
             <div class="content">
-                <h4>GIFTING FORMS</h4>
-                <h4>HELLO USERNAME</h4>
+                <h4>HELLO {{ Auth::user()->username }}</h4>
                 <h4>Send Gifting Forms by Email</h4>
                 <h4>Or <a href="{{ asset('assets/pdf/gifting-form-statement-and-non-solicitation.pdf') }}" download>CLICK
                         HERE</a> to download PDF printable forms</h4>
